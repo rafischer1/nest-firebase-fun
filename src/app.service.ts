1c8df5c61a2864537352c97bb81fcad88d9b9812
rafischer1 <artiefischer@gmail.com>
2023-11-01 16:42:59 -0400

    application loads

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'WUSSUP';
+    console.log('Listening on :3200...');
+    return 'Running...';
   }
 }
