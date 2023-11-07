f27380d3c3235fa07c347c1a8ef3e9f6636ca3e2
rafischer1 <artiefischer@gmail.com>
2023-11-07 07:57:49 -0500

    user service and dtos added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from "@nestjs/common";
+import { CreateUserDto } from "./create-user.dto";
+import { UpdateUserDto } from "./update-user.dto";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  createUser(createUserDto: CreateUserDto) {
+    console.log("create user:", createUserDto);
+    return "This action adds a new user";
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  updateUser(id: number, updateUserDto: UpdateUserDto) {
+    console.log("create user:", updateUserDto);
+    return `This action updates a #${id} user`;
+  }
+
+  deleteUser(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
